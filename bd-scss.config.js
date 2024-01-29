1850151300b0fef95f43b4cf8b3e7f7665ee9250
Skye <Riddim_GLiTCH@courvix.com>
2024-01-29 13:36:55 +0100

    Update bd-scss.config.js

--- bd-scss.config.js
+++ bd-scss.config.js
@@ -3,7 +3,7 @@ export default {
 	meta: {
 		name: "GH0STCH4TT3R",
 		author: "Riddim-GLiTCH",
-		version: "1.0.0",
+		version: "1.0.1",
 		description: "A Futuristic theme with an edgy style",
 		source: "https://github.com/Nexus-Themes/GH0STCH4TT3R",
 		invite: "aYxpgkvdvR",
